6e6cb46a7dbdcdf55611ec833ab069eb25e2de90
Samoney2005 <samone.a.cook-1@ou.edu>
2024-11-26 22:36:41 -0600

    needed to committ

--- R/ddt.R
+++ R/ddt.R
@@ -1,17 +1,16 @@
-#' Fish caught in the tennessee river and its tributaries
+#' Fish caught in the Tennessee River and its tributaries
 #'
-#' A dataset with 6 variables two of which are quantitative.
-#' Interest is in the amount of DDT in their flesh
-#'
+#' A dataset with 6 variables, two of which are quantitative.
+#' Interest is in the amount of DDT in their flesh.
 #' @format A data frame with 144 rows and 6 variables:
 #' \describe{
 #' \item{RIVER}{Three letter abbreviation of the River}
 #' \item{MILE}{Mile count for where the fish was caught}
-#'  \item{DDT}{Numeric, concentration of DDT}
-#'   \item{LENGTH}{Numeric, length of something measured}
-#'   \item{SPECIES}{Factor, species name}
-#'   \item{WEIGHT}{Numeric, weight of speices}
-#' ...
-#' }
+#' \item{DDT}{Numeric, concentration of DDT}
+#' \item{LENGTH}{Numeric, length of something measured}
+#' \item{SPECIES}{Factor, species name}
+#' \item{WEIGHT}{Numeric, weight of species}
+#' ... }
 #' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences-Sixth-Edition/Mendenhall-Sincich/p/book/9781498728850}
 "ddt"
+
